48ef277e09c28d510263954022059a0fc266a7e3
Maxim Freck <maxim@freck.pp.ru>
2023-04-29 01:33:01 +0400

    WIP

---
+++ app/ui5/persons/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(["sap/fe/core/AppComponent"], function(AppComponent) {
+    "use strict";
+
+    return AppComponent.extend("net.php.fiori.persons.persons.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
---
+++ resources/ui5/persons/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("net.php.fiori.persons.persons.Component",{metadata:{manifest:"json"}})});
